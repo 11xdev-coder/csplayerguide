22ee83b16b9beb39c5bf67f7af3ac1dbb1b7f1ab
Matvei Piashchynski <66955882+11xdev-coder@users.noreply.github.com>
2023-03-23 23:13:43 +0300

    inheritance

---
+++ book/Part2/inheritance.cs
@@ -0,0 +1,39 @@
+﻿namespace book.part2.inheritance;
+
+public class inheritance
+{
+    public static void inheritanceFunc()
+    {
+        GameObject asteroid = new Asteroid(2, 3);
+        Console.WriteLine(asteroid.PosX);
+        Console.WriteLine(asteroid.PosY);
+    }
+}
+
+public class GameObject
+{
+    public float PosX { get; set; }
+    public float PosY { get; set; }
+
+    public GameObject()
+    {
+        this.PosX = 0;
+        this.PosY = 0;
+    }
+    
+    public GameObject(float PosX, float PosY)
+    {
+        this.PosX = PosX;
+        this.PosY = PosY;
+    }
+}
+
+public class Asteroid : GameObject // includes GameObject's variables
+{
+    public float Size { get; set; }
+
+    public Asteroid(float PosX, float PosY) : base(PosX, PosY)
+    {
+        
+    }
+}
