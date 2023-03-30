822f79ba5c76747da976a6986c4637e7958cb82d
Matvei Piashchynski <66955882+11xdev-coder@users.noreply.github.com>
2023-03-30 21:20:11 +0300

    read "Records" completed challenge "war preparations"

---
+++ book/Part2/records/records.cs
@@ -0,0 +1,40 @@
+﻿namespace book.part2.records.records;
+
+public class records
+{
+    public static void Start()
+    {
+        // records are compact alternative for defining data-centric class or struct
+        // the compiler automatically generetas a constuctor and properties
+        
+        // Records automatically override ToString method
+        Console.WriteLine(new Point(2, 3).ToString()); // Point { x = 2, y = 3 }
+        
+        // Value Semantics
+        Point a = new Point(2, 3);
+        Point b = new Point(2, 3);
+        Console.WriteLine(a == b); // true
+        // Though a and b refer to different instances and use separate memory locations, this will be true
+        
+        // Deconstruction
+        Point p = new Point(2, 3);
+        (float x, float y) = p;
+        Console.WriteLine($"{x} {y}");
+        
+        // with Statements
+        // records give you extra form of a with statement
+        Point p1 = new Point(-2, 5);
+        Point p2 = p1 with {x = 0};
+        Console.WriteLine($"{p1.ToString()} {p2.ToString()}"); // Point { x = -2, y = 5 } Point { x = 0, y = 5 }
+        // replace one value of record
+    }
+}
+
+public record Point(float x, float y); // thats all
+
+// additional members
+public record Rectangle(float width, float height)
+{
+    public float Area => width * height;
+}
+
---
+++ book/Part2/records/warPreparations.cs
@@ -0,0 +1,17 @@
+﻿namespace book.part2.records.warPreparations;
+
+public class warPreparations
+{
+    public static void Start()
+    {
+        Sword basicSword = new Sword(SwordMaterial.iron, GemStone.None, 25f, 1.5f);
+        Sword sword1 = basicSword with { gemstone = GemStone.amber };
+        Sword sword2 = basicSword with { material = SwordMaterial.bronze, length = 60f };
+        Console.WriteLine($"basic sword: {basicSword.ToString()} sword1: {sword1} sword2: {sword2}");
+    }
+}
+
+public record Sword(SwordMaterial material, GemStone gemstone, float length, float crossguardWidth);
+
+public enum SwordMaterial { wood, bronze, iron, steel, binarium }
+public enum GemStone { None, emerald, amber, sapphire, diamond, bitstone }
