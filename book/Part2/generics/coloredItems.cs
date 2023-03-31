c33c463fdeff824a5e13d03f462c7f42461ae0da
Matvei Piashchynski <66955882+11xdev-coder@users.noreply.github.com>
2023-03-31 20:26:29 +0300

    read level "Generics" completed challenge "Colored Items"

---
+++ book/Part2/generics/coloredItems.cs
@@ -0,0 +1,48 @@
+﻿namespace book.part2.generics.coloredItems;
+
+public class coloredItems
+{
+    public static void Start()
+    {
+        ColoredItem<Sword> sword = new ColoredItem<Sword>(new Sword(), ConsoleColor.Blue);
+        ColoredItem<Bow> bow = new ColoredItem<Bow>(new Bow(), ConsoleColor.Red);
+        ColoredItem<Axe> axe = new ColoredItem<Axe>(new Axe(), ConsoleColor.Green);
+
+        sword.Display();
+        bow.Display();
+        axe.Display();
+    }
+}
+
+public class ColoredItem<T>
+{
+    public T Item { get; }
+    public ConsoleColor Color { get; }
+
+    public ColoredItem(T Item, ConsoleColor Color)
+    {
+        this.Item = Item;
+        this.Color = Color;
+    }
+
+    public void Display()
+    {
+        Console.ForegroundColor = Color;
+        Console.WriteLine(Item.ToString());
+    }
+}
+
+public class Sword
+{
+    public override string ToString() => "Sword";
+}
+
+public class Bow
+{
+    public override string ToString() => "Bow";
+}
+
+public class Axe
+{
+    public override string ToString() => "Axe";
+}
---
+++ book/Part2/generics/generics.cs
@@ -0,0 +1,50 @@
+﻿namespace book.part2.generics.generics;
+
+public class generics
+{
+    public static void Start()
+    {
+        /*
+         Generics solve the problem of making classes or methods that would differ only by the types
+         they use.
+         Defining generic class: public class List<T> { public T GetItemAt(int index) { ... } ... }
+         You can also make generic methods and generic types with multiple type parameters
+         */
+
+        List<int> numbers = new List<int>();
+        numbers.Add(1);
+        numbers.Add(2);
+        for (int index = 0; index < numbers._items.Length; index++)
+        {
+            Console.WriteLine(numbers.GetItemAt(index));
+        }
+            
+    }
+}
+// Generic List class
+public class List<T>
+{
+    public T[] _items = new T[0];
+
+    public T GetItemAt(int index) => _items[index];
+    public void SetItemAt(int index, T value) => _items[index] = value;
+
+    public void Add(T newValue)
+    {
+        T[] updated = new T[_items.Length + 1];
+
+        for (int index = 0; index < _items.Length; index++)
+            updated[index] = _items[index];
+
+        updated[^1] = newValue;
+
+        _items = updated;
+    }
+}
+
+public class GameObject { }
+
+public class Asteroid { }
+
+// generic type constraint
+public class GenericType<T, U> where T : GameObject where U : Asteroid { }
