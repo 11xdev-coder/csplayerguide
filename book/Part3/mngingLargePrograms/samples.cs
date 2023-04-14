d60e6f86ff29eabec9864bf4e72892e3feae520a
Matvei Piashchynski <66955882+11xdev-coder@users.noreply.github.com>
2023-04-14 23:38:24 +0300

    read "Managing Larger Programs" article

---
+++ book/Part3/mngingLargePrograms/samples.cs
@@ -0,0 +1,16 @@
+﻿namespace book.part3.mngingLargePrograms.samples;
+
+using static System.Math;
+
+public class samples
+{
+    public static void Start()
+    {
+        // multiple files are good
+        
+        // namespaces and using directives
+        // namespace is a named container for organizing types
+        System.Console.WriteLine("using namespace");
+        double pi = PI; // without writing the name (using static)
+    }
+}
