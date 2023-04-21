9091741fa5ad2e3d1b574f8d26e0d796538995b5
Matvei Piashchynski <66955882+11xdev-coder@users.noreply.github.com>
2023-04-21 21:06:08 +0300

    made "Exepti's Game"

---
+++ book/Part3/exceptions/exeptisGame.cs
@@ -0,0 +1,42 @@
+﻿namespace book.part3.exceptions.exeptisGame;
+
+public class exeptisGame
+{
+    public static void Start()
+    {
+        Random _random = new Random();
+        int initialNumber = _random.Next(0, 10); // between 0 and 9
+        int userNumber;
+        List<int> usedNumbers = new List<int>();
+        
+        while (true)
+        {
+            Console.WriteLine("Pick a number between 0 and 9");
+            try
+            {
+                userNumber = Convert.ToInt32(Console.ReadLine());
+                if (userNumber >= 0 & userNumber <= 9)
+                {
+                    if (!usedNumbers.Contains(userNumber))
+                    {
+                        if (userNumber == initialNumber) break;
+                    }
+                    else Console.WriteLine("That number was already inputed");
+                    
+                    usedNumbers.Add(userNumber);
+                }
+                else Console.WriteLine("Number wasnt in range");
+            }
+            catch
+            {
+                continue;
+            }
+        }
+        
+        Console.Write("Used Numbers: ");
+        foreach (int number in usedNumbers)
+        {
+            Console.Write($"{number} ");
+        }
+    }
+}
