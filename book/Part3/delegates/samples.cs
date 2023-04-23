d0c7917144acb44f5d75fe1e1d66b38540b561f8
Matvei Piashchynski <66955882+11xdev-coder@users.noreply.github.com>
2023-04-23 22:12:11 +0300

    read "Delegates" made "The Sieve"

---
+++ book/Part3/delegates/samples.cs
@@ -0,0 +1,31 @@
+﻿namespace book.part3.delegates.samples;
+
+public class samples
+{
+    public delegate int NumberDelegate(int number);
+    
+    public static void Start()
+    {
+        // delegate is a variable which stores a reference to a method
+        // define delegate: public delegate float SomeFunc(args args)
+        // assign values to delegate variables: SomeFunc d = AddOne
+        // invoke method stored in delegate variable: d(2) or d.Invoke(2)
+        // Action, Func, and Predicate are pre-defined generic delegate types
+        // Delegates can refer to multiple methods and call them in turn
+        
+        // sample how delegate works
+        int[] ChangeArrayElements(int[] numbers, NumberDelegate operation)
+        {
+            int[] result = new int[numbers.Length];
+
+            for (int index = 0; index > result.Length; index++)
+                result[index] = operation(numbers[index]); // imagine if there instead of "operation" stands something like "AddOne"; we are calling a reference to a method
+
+            return result;
+        }
+
+        int AddOne(int number) => number + 1;
+        
+        ChangeArrayElements(new [] {1, 2, 3, 4, 5}, AddOne);
+    }
+}
---
+++ book/Part3/delegates/theSieve.cs
@@ -0,0 +1,71 @@
+﻿namespace book.part3.delegates.theSieve;
+
+public class theSieve
+{
+    public static void Start()
+    {
+        Sieve sieve = new Sieve();
+        string userInput;
+        int userNumber;
+
+        while (true)
+        {
+            Console.WriteLine("Enter filter(Even, Positive, Multiplies 10)");
+            userInput = Console.ReadLine();
+
+            if (userInput == "Even")
+            {
+                sieve.filter = FilterType.Even;
+                break;
+            }
+            if (userInput == "Positive")
+            {
+                sieve.filter = FilterType.Positive;
+                break;
+            }
+            if (userInput == "Multiplies 10")
+            {
+                sieve.filter = FilterType.Multiplies10;
+                break;
+            }
+            else continue;
+        }
+        
+        while (true)
+        {
+            Console.WriteLine("Enter da number");
+            userInput = Console.ReadLine();
+            if (!int.TryParse(userInput, out userNumber))
+            {
+                Console.WriteLine("Cannot parse user input");
+                continue;
+            }
+            
+            Console.WriteLine(sieve.IsGood(userNumber));
+        }
+    }
+}
+
+public class Sieve
+{
+    public FilterType filter { get; set; }
+
+    public bool IsGood(int number)
+    {
+        if (filter == FilterType.Even)
+            if (number % 2 == 0)
+                return true;
+        
+        if (filter == FilterType.Positive)
+            if (number > 0)
+                return true;
+        
+        if (filter == FilterType.Multiplies10)
+            if (number % 10 == 0)
+                return true;
+
+        return false;
+    }
+}
+
+public enum FilterType { Even, Positive, Multiplies10 }
