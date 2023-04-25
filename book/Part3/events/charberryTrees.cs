b04a44015f7780b6c4e26609b53caccbdf67ed92
Matvei Piashchynski <66955882+11xdev-coder@users.noreply.github.com>
2023-04-25 20:46:14 +0300

    read "Events" made "Charberry trees"

---
+++ book/Part3/events/charberryTrees.cs
@@ -0,0 +1,59 @@
+﻿namespace book.part3.events.charberryTrees;
+
+public class charberryTrees
+{
+    public static void Start()
+    {
+        CharberryTree tree = new CharberryTree();
+        Notifier notifier = new Notifier(tree);
+        Harvester harvester = new Harvester(tree);
+        
+        while(true)
+            tree.MaybeGrow();
+    }
+}
+
+public class CharberryTree
+{
+    private Random _random = new Random();
+    public bool Ripe { get; set; }
+
+    public event Action TreeRipened;
+
+    public void MaybeGrow()
+    {
+        // only a tiny chance of ripening
+        if (_random.NextDouble() < 0.00000001 && !Ripe)
+        {
+            Ripe = true;
+            TreeRipened?.Invoke();
+        }
+    }
+}
+
+public class Notifier
+{
+    public Notifier(CharberryTree tree)
+    {
+        tree.TreeRipened += OnFruitRipe;
+    }
+
+    public void OnFruitRipe() => Console.WriteLine("A charberry fruit has ripened!");
+}
+
+public class Harvester
+{
+    private CharberryTree tree;
+    
+    public Harvester(CharberryTree tree)
+    {
+        this.tree = tree;
+        this.tree.TreeRipened += Harvest;
+    }
+
+    public void Harvest()
+    {
+        this.tree.Ripe = false;
+        Console.WriteLine("Tree has been harvested");
+    }
+}
--- book/Part3/events/samples.cs
+++ book/Part3/events/samples.cs
@@ -1,4 +1,6 @@
-﻿namespace book.part3.events.samples;
+﻿using book.part2.records.records;
+
+namespace book.part3.events.samples;
 
 public class samples
 {
@@ -6,4 +8,46 @@ public class samples
     {
         
     }
+}
+
+public class Ship
+{
+    // defining an event
+    public event Action? shipExploded;
+    
+    // evemts with parameters
+    public event Action<Point>? shipExplodedWithLocation;
+    
+    public int Health { get; set; }
+    public Point Location { get; set; } = new Point(0, 0);
+
+    public void TakeDamage(int amount)
+    {
+        Health -= amount;
+        // call an event
+        if (Health <= 0)
+        {
+            shipExploded(); // or shipExploded.Invoke();
+            shipExplodedWithLocation(Location);
+            
+            // null events
+            shipExploded?.Invoke();
+        }
+        
+    }
+}
+
+public class SoundEffectManager
+{
+    public SoundEffectManager(Ship ship)
+    {
+        ship.shipExplodedWithLocation += OnShipExplode; // this attaches or subscribes the OnShipExplode method to the event
+        // unsubscribing or detaching is like this:
+        // ship.shipExploded -= OnShipExplode;
+    }
+    
+    public void OnShipExplode(Point location) => PlaySound("Explosion", CalculateVolume(location));
+
+    public float CalculateVolume(Point location) => (location.x + location.y) / 2;
+    public void PlaySound(string name, float volume) { Console.WriteLine(name); }
 }
