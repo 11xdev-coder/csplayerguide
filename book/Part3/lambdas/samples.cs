30edd425de510c8afa21bc41620ba34e71c9ab57
Matvei Piashchynski <66955882+11xdev-coder@users.noreply.github.com>
2023-04-27 23:45:08 +0300

    made "The Lambda Sieve"

---
+++ book/Part3/lambdas/samples.cs
@@ -0,0 +1,34 @@
+﻿using System.Globalization;
+
+namespace book.part3.lambdas.samples;
+
+public class samples
+{
+    public static int Count(int[] input, Func<int, bool> countFunction)
+    {
+        int count = 0;
+
+        foreach (int number in input)
+            if (countFunction(number))
+                count++;
+
+        return count;
+    }
+    
+    public static void Start()
+    {
+        // basic sample how lambdas work
+        // they are basically small functions
+        // in parantheses () you write arguments
+        // in {} you write function code
+        // then you can call it by its name
+        var hello = () => { Console.WriteLine("hello"); };
+        hello();
+        var sum = (int x, int y) => { Console.WriteLine($"{x} + {y} = {x + y}"); };
+        sum(1, 2);
+
+        int[] numbers = new[] {2, 3, 4, 5};
+        int count = Count(numbers, n => n % 2 == 0); // mini function that defines number is even or no
+        Console.WriteLine(count);
+    }
+}
---
+++ book/Part3/lambdas/theLambdaSieve.cs
@@ -0,0 +1,63 @@
+﻿namespace book.part3.lambdas.theLambdaSieve;
+
+public class theLambdaSieve
+{
+    
+    
+    public static void Start()
+    {
+        string userInput;
+        int userNumber;
+        Sieve sieve;
+
+        while (true)
+        {
+            Console.WriteLine("Enter filter(1 = Even, 2 = Positive, 3 = MultipleOfTen)");
+            userInput = Console.ReadLine();
+            try
+            {
+                sieve = userInput switch
+                {
+                    "1" => new Sieve(IsEven),
+                    "2" => new Sieve(IsPositive),
+                    "3" => new Sieve(MultipleOfTen)
+                };
+            }
+            catch
+            {
+                continue;
+            }
+
+            break;
+        }
+        
+        while (true)
+        {
+            Console.WriteLine("Enter da number");
+            userInput = Console.ReadLine();
+            if (!int.TryParse(userInput, out userNumber))
+            {
+                Console.WriteLine("Cannot parse user input");
+                continue;
+            }
+            
+            Console.WriteLine(sieve.IsGood(userNumber));
+        }
+        
+        bool IsEven(int number) => number % 2 == 0;
+        bool IsPositive(int number) => number >= 0;
+        bool MultipleOfTen(int number) => number % 10 == 0;
+    }
+}
+
+public class Sieve
+{
+    private Func<int, bool> _desicionFunc;
+
+    public Sieve(Func<int, bool> _desicionFunc)
+    {
+        this._desicionFunc = _desicionFunc;
+    }
+
+    public bool IsGood(int number) => _desicionFunc(number);
+}
