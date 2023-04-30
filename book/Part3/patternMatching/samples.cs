21ae70abce4ef65a6d5c7347fc94d190b6ab2e00
Matvei Piashchynski <66955882+11xdev-coder@users.noreply.github.com>
2023-04-30 21:49:03 +0300

    read "Matching patterns" made "The Potion Master of Pattren"

---
+++ book/Part3/patternMatching/samples.cs
@@ -0,0 +1,39 @@
+﻿namespace book.part3.patternMatching.samples;
+
+public class samples
+{
+    public static void Start()
+    {
+        // the monster scoring problem
+        int ScoreFor(Monster monster)
+        {
+            // type pattern
+            return monster switch
+            {
+                // and, or, not keywords are allowed on patterns
+                Snake s1 when s1.length >= 3 => 7, // case guard, put always on top
+                Snake s => (int)(s.length * 2), // declaration pattern
+                Dragon { phase: LifePhase.Ancient} => 100, // property pattern, you can add multiple properties
+                Dragon => 50,
+                Orc { sword: { type: SwordType.WoodenStick} } => 2, // nested pattern
+                Orc { sword.type: SwordType.Longsword} => 15, // nested pattern
+                _ => 5
+            };
+        }
+    }
+}
+
+public abstract record Monster;
+
+public record Skeleton() : Monster;
+
+public record Snake(double length) : Monster;
+
+public record Dragon(DragonType type, LifePhase phase) : Monster;
+
+public record Orc(Sword sword) : Monster;
+public record Sword(SwordType type);
+
+public enum DragonType { Black, Green, Red, Blue, Gold }
+public enum LifePhase { Wyrmling, Young, Adult, Ancient }
+public enum SwordType { WoodenStick, ArmingSword, Longsword }
---
+++ book/Part3/patternMatching/thePotionMastersOfPattren.cs
@@ -0,0 +1,53 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace book.part3.patternMatching.thePotionMastersOfPattren;
+
+public class thePotionMastersOfPattren
+{
+    public static void Start()
+    {
+        PotionType potion = PotionType.water;
+        
+        while (true)
+        {
+            Console.WriteLine($"Current potion is {potion}");
+            
+            Console.WriteLine("Do you want to continue making your potion?");
+            string? input = Console.ReadLine();
+            if (input == "no") break;
+            
+            Console.WriteLine("Available ingridients: stardust, snake venom, dragon breath, shadow glass, eyeshine gem");
+
+            Ingridient ingridient = Console.ReadLine() switch
+            {
+                "stardust" => Ingridient.stardust,
+                "snake venom" => Ingridient.snakeVenom,
+                "dragon breath" => Ingridient.dragonBreath,
+                "shadow glass" => Ingridient.shadowGlass,
+                "eyeshine gem" => Ingridient.eyeshineGem
+            };
+
+            potion = (ingridient, potion) switch
+            {
+                (Ingridient.stardust, PotionType.water) => PotionType.elixir,
+                (Ingridient.snakeVenom, PotionType.elixir) => PotionType.poison,
+                (Ingridient.dragonBreath, PotionType.elixir) => PotionType.flying,
+                (Ingridient.shadowGlass, PotionType.elixir) => PotionType.invisibility,
+                (Ingridient.eyeshineGem, PotionType.elixir) => PotionType.nightSight,
+                (Ingridient.shadowGlass, PotionType.nightSight) => PotionType.cloudy,
+                (Ingridient.eyeshineGem, PotionType.invisibility) => PotionType.cloudy,
+                (Ingridient.stardust, PotionType.cloudy) => PotionType.wraith,
+                (_, _) => PotionType.ruined
+            };
+            
+            if (potion == PotionType.ruined)
+            {
+                Console.WriteLine("your potion is ruined, start over nub!!!!!!11!");
+                potion = PotionType.water;
+            }
+        }
+    }
+}
+
+public enum PotionType { water, elixir, poison, flying, invisibility, nightSight, cloudy, wraith, ruined }
+public enum Ingridient { stardust, snakeVenom, dragonBreath, shadowGlass, eyeshineGem }
