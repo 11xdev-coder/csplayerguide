b85b4395b459867f39842761e0b652a1ead7f773
Matvei Piashchynski <66955882+11xdev-coder@users.noreply.github.com>
2023-05-01 22:54:05 +0300

    read "Operator overloading" made "Navigating operand city"

---
+++ book/Part3/operatorOverloading/navigatingOperandCity.cs
@@ -0,0 +1,67 @@
+﻿namespace book.part3.operatorOverloading.navigatingOperandCity;
+
+public class navigatingOperandCity
+{
+    public static void Start()
+    {
+        BlockCoordinate a = new BlockCoordinate(5, 4);
+        BlockOffset b = new BlockOffset(1, 1);
+        BlockCoordinate c = a + b;
+        Console.WriteLine($"Coordinate + offset: {c.row}, {c.column}");
+        
+        BlockCoordinate d = new BlockCoordinate(5, 4);
+        Direction e = Direction.North;
+        BlockCoordinate f = d + e;
+        Console.WriteLine($"Coordinate + direction: {f.row}, {f.column}");
+        
+        Console.WriteLine($"Indexers: C: {c[0]}, {c[1]}. f: {f[0]}, {f[1]}");
+
+        BlockOffset h = e;
+        Console.WriteLine($"From direction to blockoffset: {h.rowOffset}, {h.columnOffset}");
+    }
+}
+
+public record BlockCoordinate(int row, int column)
+{
+    public static BlockCoordinate operator +(BlockCoordinate coordinate, BlockOffset offset) =>
+        new BlockCoordinate(coordinate.row + offset.rowOffset, coordinate.column + offset.columnOffset);
+
+    public static BlockCoordinate operator +(BlockCoordinate coordinate, Direction direction)
+    {
+        BlockCoordinate newCoordinate = direction switch
+        {
+            Direction.North => new BlockCoordinate(coordinate.row + 1, coordinate.column),
+            Direction.South => new BlockCoordinate(coordinate.row - 1, coordinate.column),
+            Direction.East => new BlockCoordinate(coordinate.row, coordinate.column + 1),
+            Direction.West => new BlockCoordinate(coordinate.row, coordinate.column - 1)
+        };
+
+        return newCoordinate;
+    }
+
+    public int this[int index]
+    {
+        get
+        {
+            if (index == 0) return row;
+            else return column;
+        }
+    }
+}
+
+public record BlockOffset(int rowOffset, int columnOffset)
+{
+    public static implicit operator BlockOffset(Direction direction) // from direction to blockoffset
+    {
+        BlockOffset newOffset = direction switch
+        {
+            Direction.North => new BlockOffset(1, 0),
+            Direction.South => new BlockOffset(-1, 0),
+            Direction.East => new BlockOffset(0, 1),
+            Direction.West => new BlockOffset(0, -1)
+        };
+
+        return newOffset;
+    }
+}
+public enum Direction { North, East, South, West }
---
+++ book/Part3/operatorOverloading/samples.cs
@@ -0,0 +1,74 @@
+﻿namespace book.part3.operatorOverloading.samples;
+
+public class samples
+{
+    public static void Start()
+    {
+        // result
+        Point a = new Point(2, 8);
+        Point b = new Point(5, 2);
+
+        Point result = a + b;
+        Console.WriteLine($"{result.X}, {result.Y}");
+        
+        // indexers
+        Pair pair = new Pair();
+        pair[0] = 1;
+        pair[1] = 4; // same
+        Pair pair2 = new Pair()
+        {
+            [0] = 1,
+            [1] = 4,
+        }; // same
+        
+        Console.WriteLine($"{pair[0]}, {pair[1]}");
+        Console.WriteLine($"{pair2[0]}, {pair2[1]}");
+
+        Point2 c = new Point2(1, 2);
+        Point3 d = c; // implicit
+
+        Point3 e = new Point3(1, 2, 3);
+        Point2 f = (Point2)e; // explicit
+
+        int g = 0;
+        long h = g; // implicit
+        int j = (int)h; // explicit
+    }
+}
+
+public record Point(double X, double Y)
+{
+    // defining an operator overload
+    public static Point operator +(Point a, Point b) => new Point(a.X + b.X, a.Y + b.Y);
+    // they must be marked with public static
+}
+// example of indexer
+public class Pair
+{
+    public int first { get; set; }
+    public int second { get; set; }
+
+    public int this[int index]
+    {
+        get
+        {
+            if (index == 0) return first;
+            else return second;
+        }
+        set
+        {
+            if (index == 0) first = value;
+            else second = value;
+        }
+    }
+}
+
+public record Point2(int X, int Y)
+{
+    public static explicit operator Point2(Point3 p) => new Point2(p.X, p.Y); // conversion from point3 to point2
+}
+
+public record Point3(int X, int Y, int Z)
+{
+    public static implicit operator Point3(Point2 p) => new Point3(p.X, p.Y, 0); // conversion from point2 to point3
+}
