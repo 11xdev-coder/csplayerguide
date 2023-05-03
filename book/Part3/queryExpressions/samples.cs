93e4d9d82adde6f471863baf557ccf1b5a79ab6b
Matvei Piashchynski <66955882+11xdev-coder@users.noreply.github.com>
2023-05-03 21:31:48 +0300

    read "Query Expressions" made "The Three Lenses"

---
+++ book/Part3/queryExpressions/samples.cs
@@ -0,0 +1,66 @@
+﻿namespace book.part3.queryExpressions.samples;
+
+public class samples
+{
+    public static void Start()
+    {
+        void PrintFromQuery<T>(IEnumerable<T> enumerable)
+        {
+            foreach (var text in enumerable)
+                Console.WriteLine(text);
+        }
+        // query expressions are a special type of statement that allows you to extract specific pieces from data
+        // query expressions are made of multiple clauses
+
+        List<GameObject> objects = new List<GameObject>();
+        objects.Add(new Ship {ID = 1, x=0, y=0, HP=50, MaxHP=100});
+        objects.Add(new Ship {ID = 2, x=4, y=2, HP=75, MaxHP=100});
+        objects.Add(new Ship {ID = 3, x=9, y=3, HP=0, MaxHP=100});
+
+        // from identifies the collection that is being queried
+        // select identifies data to be produced
+        IEnumerable<string> healthTexts = from o in objects select $"{o.HP}/{o.MaxHP}"; // creates object "o", selecting data
+                                                                        // collection and extracting information
+                                                                        // we need from object "o"
+        PrintFromQuery(healthTexts); // queries are IEnumerable<type>
+        
+        // filtering
+        Console.WriteLine("ID's of alive ships:");
+        var aliveObjects = from o in objects where o.HP > 0 select o.ID; // creating object o in objects, 
+                                                                         // sorting out their hp,
+                                                                         // and extracting ids of ships hp > 0
+                                                                         // there can be more than 1 where's at a time!
+        PrintFromQuery(aliveObjects);
+        // sorting
+        Console.WriteLine("Every ship's hp sorted from smaller to bigger:");
+        var weakierObjects = from o in objects orderby o.HP select o.ID; // there can be more params to sort
+        PrintFromQuery(weakierObjects);
+        Console.WriteLine("Every ship's hp sorted from bigger to smaller:");
+        var healthierObjects = from o in objects 
+            orderby o.HP descending 
+            select o.ID; // there can be more params to sort
+        PrintFromQuery(healthierObjects);
+        
+        // method syntax
+        Console.WriteLine("Alive ships sortered by hp(method syntax):");
+        var results = objects
+            .Where(o => o.HP > 0)
+            .OrderBy(o => o.HP)
+            .Select(o => o.HP);
+        PrintFromQuery(results);
+    }
+}
+
+public class GameObject
+{
+    public int ID { get; set; }
+    public double x { get; set; }
+    public double y { get; set; }
+    public int MaxHP { get; set; }
+    public int HP { get; set; }
+}
+
+public class Ship : GameObject
+{
+    
+}
---
+++ book/Part3/queryExpressions/theThreeLenses.cs
@@ -0,0 +1,56 @@
+﻿namespace book.part3.queryExpressions.theThreeLenses;
+
+public class theThreeLenses
+{
+    public static void Start()
+    {
+        int[] input = new[] {1, 9, 2, 8, 3, 7, 4, 6, 5};
+
+        
+        foreach (int number in SortMethod(input))
+        {
+            Console.WriteLine(number);
+        }
+        
+        foreach (int number in SortQueryKeyword(input))
+        {
+            Console.WriteLine(number);
+        }
+        
+        foreach (int number in SortQueryMethod(input))
+        {
+            Console.WriteLine(number);
+        }
+
+        IEnumerable<int> SortMethod(int[] input)
+        {
+            List<int> filtered = new List<int>();
+
+            foreach (int number in input)
+            {
+                if(number % 2 == 0) filtered.Add(number * 2);
+            }
+
+            int[] results = filtered.ToArray();
+            Array.Sort(results);
+
+            return results;
+        }
+
+        IEnumerable<int> SortQueryKeyword(int[] input)
+        {
+            return from n in input
+                where n % 2 == 0
+                orderby n
+                select n * 2;
+        }
+        
+        IEnumerable<int> SortQueryMethod(int[] input)
+        {
+            return input
+                .Where(n => n % 2 == 0)
+                .OrderBy(n => n)
+                .Select(n => n * 2);
+        }
+    }
+}
