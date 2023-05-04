86f800faf6c12133f626908e487329bdcd3e6676
Matvei Piashchynski <66955882+11xdev-coder@users.noreply.github.com>
2023-05-04 21:54:52 +0300

    read "Threads" made the challenge

---
+++ book/Part3/threads/samples.cs
@@ -0,0 +1,26 @@
+﻿namespace book.part3.threads.samples;
+
+public class samples
+{
+    public static void Start()
+    {
+        // creating threads allows your program to do more than one thing at a  time:
+        // Thread thread = new Thread(MethodName); thread.Start();
+        // pass something to a thread:
+        // thread.Start(ObjectName);
+        // wait for a thread to finish:
+        // thread.Join();
+
+        void Count()
+        {
+            for (int index = 0; index < 25; index++)
+                Console.WriteLine(index);
+        }
+        Thread thread = new Thread(Count);
+        thread.Start();
+        thread.Join();
+        Console.WriteLine("Thread dun");
+        
+        Thread.Sleep(1000); // 1 second
+    }
+}
---
+++ book/Part3/threads/theRepeatingStream.cs
@@ -0,0 +1,50 @@
+﻿namespace book.part3.threads.theRepeatingStream;
+
+public class theRepeatingStream
+{
+    public static void Start()
+    {
+        void StartNumbersGen(RecentNumbers? obj)
+        {
+            if (obj == null || obj is not RecentNumbers) return;
+            
+            RecentNumbers recentNumbers = (RecentNumbers)obj;
+            Random random = new Random();
+            while (true)
+            {
+                int nextNumber = random.Next(10);
+
+                lock (recentNumbers)
+                {
+                    recentNumbers.recentNumber2 = recentNumbers.recentNumber1;
+                    recentNumbers.recentNumber1 = nextNumber;
+                }
+
+                Console.WriteLine($"Number Generated: {nextNumber}");
+                Thread.Sleep(1000);
+            }
+        }
+
+        RecentNumbers recentNumbers = new RecentNumbers() { recentNumber1 = -1, recentNumber2 = -2 };
+        Thread generatingThread = new Thread(() => StartNumbersGen(recentNumbers));
+        generatingThread.Start();
+        
+        while (true)
+        {
+            Console.ReadKey(false);
+
+            bool isDuplicate;
+            lock(recentNumbers)
+                isDuplicate = recentNumbers.recentNumber1 == recentNumbers.recentNumber2;
+    
+            if(isDuplicate) Console.WriteLine("You found a duplicate!");
+            else Console.WriteLine("That is not a duplicate.");
+        }
+    }
+}
+
+public class RecentNumbers
+{
+    public int recentNumber1 { get; set; }
+    public int recentNumber2 { get; set; }
+}
