56c7c30732702585b8033dc0b6c198c415ff9ded
Matvei Piashchynski <66955882+11xdev-coder@users.noreply.github.com>
2023-05-06 23:24:31 +0300

    read "Async Programming" and made challenges:

---
+++ book/Part3/asyncProgramming/samples.cs
@@ -0,0 +1,20 @@
+﻿namespace book.part3.asyncProgramming.samples;
+
+public class samples
+{
+    public static void Start()
+    {
+        void AddOnEurope(int a, int b, Action<int> callback)
+        {
+            Thread thread = new Thread(() =>
+            {
+                Thread.Sleep(3000);
+                int result = a + b;
+                callback(result);
+            });
+            thread.Start();
+        }
+
+        AddOnEurope(2, 3, result => Console.WriteLine(result)); // callback
+    }
+}
