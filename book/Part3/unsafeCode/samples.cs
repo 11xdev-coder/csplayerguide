f63005642089e11594c92c50a0e6dc8b945bed18
Matvei Piashchynski <66955882+11xdev-coder@users.noreply.github.com>
2023-05-08 20:33:25 +0300

    read "Unsafe Code"

---
+++ book/Part3/unsafeCode/samples.cs
@@ -0,0 +1,26 @@
+﻿namespace book.part3.unsafeCode.samples;
+
+
+public class samples
+{
+    public unsafe static void Start()
+    {
+        // actions that allow you to modify data directly is called unsafe code
+        // unsafe { do unsafe actions here }
+        int x;
+        unsafe
+        {
+            int* p; // pointer to an integer 
+            
+            // Address-of operator. gets address of variable and returns it
+            // get an address to int x
+            int* pointerToX = &x;
+            
+            // indirection operator
+            *pointerToX = 3; // same as x = 3;
+            
+            // pointer member access operator: allows access to members through a pointer
+            pointerToX->GetType(); // same as x.GetType();
+        }
+    }
+}
